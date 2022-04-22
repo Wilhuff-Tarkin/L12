bf260c0ef11815018188bb3cf69e974901740f4e
Grzegorz Ko <giantcaterpillar@gmail.com>
2022-04-22 15:55:18 +0200

    need to fix selectors on sliders

--- src/main/java/pages/FilterPage.java
+++ src/main/java/pages/FilterPage.java
@@ -1,9 +1,14 @@
 package pages;
 
+import ch.qos.logback.core.joran.conditional.ThenAction;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.lang.invoke.SwitchPoint;
 
 public class FilterPage extends BasePage {
 
@@ -17,24 +22,84 @@ public class FilterPage extends BasePage {
     private WebElement firstFilterOnPage;
 
     @Getter
-    @FindBy(css = "#slider-range_98591 :nth-child(2)")
+    @FindBy(css = "#slider-range_35648 > a:nth-of-type(1)")
     private WebElement sliderHandleLeft;
 
     @Getter
-    @FindBy(css = "#slider-range_98591 :nth-child(3)")
+    @FindBy(css = "#slider-range_35648 > a:nth-of-type(2)")
     private WebElement sliderHandleRight;
 
+    @Getter
+    @FindBy(css = "li>p")
+    private WebElement priceScope;
 
     public FilterPage(WebDriver driver) {
         super(driver);
     }
 
-    public void setPriceFilter (int from, int to){
+    public void setPriceFilter(int from, int to) {
+        int minPriceValue = getPriceScope(0);
+        int maxPriceValue = getPriceScope(1);
+
+        if (from > minPriceValue && from < maxPriceValue) {
+            operatePriceSlider(sliderHandleLeft, from, SliderDirection.RIGHT);
+        }
+
+        if (to < maxPriceValue && to > from) {
+            operatePriceSlider(sliderHandleRight, to, SliderDirection.LEFT);
+        }
 
     }
 
-    public void closeFilter (){
-                closeFilterBtn.click();
+    private void operatePriceSlider(WebElement sliderHandle, int price, SliderDirection direction) {
+
+        int offset = 0;
+        int postion = 0;
+
+        switch (direction){
+
+            case RIGHT:
+                while (postion != price) {
+                    offset = 10;
+                    moveSliderByOffset(offset, sliderHandle);
+                    postion = getPriceScope(0);
+
+                }
+            case LEFT:
+                while (postion != price) {
+                    offset = -10;
+                    moveSliderByOffset(offset, sliderHandle);
+                    postion = getPriceScope(1);
+                }
+        }
+    }
+
+    private void moveSliderByOffset(int offset, WebElement sliderHandle) {
+        Actions builder = new Actions(driver);
+        builder.dragAndDropBy(sliderHandle, offset, 0).perform();
+        wait.until(ExpectedConditions.elementToBeClickable(sliderHandle));
+    }
+
+    public void closeFilter() {
+        closeFilterBtn.click();
+    }
+
+    public int getPriceScope(int position) {
+        String[] scopeLabels = priceScope.getText().split(" - ");
+
+        for (int i = 0; i < scopeLabels.length; i++) {
+            scopeLabels[i] = scopeLabels[i].replaceAll("[^0-9]", "");
+            scopeLabels[i] = scopeLabels[i].substring(0, scopeLabels[i].length() - 2);
+        }
+
+        return Integer.parseInt(scopeLabels[position]);
+
+    }
+
+
+    public enum SliderDirection {
+        LEFT,
+        RIGHT,
     }
 
 }
--- src/test/java/test/Filter.java
+++ src/test/java/test/Filter.java
@@ -2,6 +2,8 @@ package test;
 
 import base.TestBase;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pages.CategoryPage;
@@ -12,18 +14,25 @@ public class Filter extends TestBase {
     private static final Logger log = LoggerFactory.getLogger("filter");
 
 
-    @Test
-    void priceFiltersShouldWork() throws InterruptedException {
+    @ParameterizedTest
+    @CsvSource({
+            "9, 15",
+            "12, 29"
+    })
+    void priceFiltersShouldWork(int from, int to){
 
         HeaderPage headerPage = new HeaderPage(driver);
         headerPage.getArt().click();
+        CategoryPage artPage = new CategoryPage(driver);
 
-        CategoryPage categoryPage = new CategoryPage(driver);
+        artPage.getFilterPage().setPriceFilter(from, to);
 
-        categoryPage.getFilterPage().getFirstFilterOnPage().click();
-        Thread.sleep(1211);
-        categoryPage.getFilterPage().closeFilter();
-        Thread.sleep(1211);
+//        System.out.println("from " + artPage.getFilterPage().getPriceScope(0));
+//        System.out.println("to " + artPage.getFilterPage().getPriceScope(1));
+
+
+
+
 
     }
 }
